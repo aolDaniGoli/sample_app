5d6fa43a90cf1f3fb33c9108f7f0ee3cdd21f1d5
ggolinsk <gregory.golinsky@teamaol.com>
2012-10-12 10:45:55 -0400

    Modified flash and error display, created additional tests

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,16 @@
 module UsersHelper
   
   # Returns Gravatar for given user
-  def gravatar_for(user)
+  def gravatar_for(user, options = { size: 50 } )
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    
+    size = options[:size]
+    #If size is too large or small, reset to default of 50
+    if size < 25 && size > 350 
+      size = 50
+    end
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    
     image_tag(gravatar_url, alt: "#{user.name}'s profile picture", class: "gravatar")
   end
 end
